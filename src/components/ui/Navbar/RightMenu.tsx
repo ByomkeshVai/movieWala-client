ad07a69c892a83b5b03fc9d4695f60d1c769b0ec
byomkesh2580 <habibthemerchant@gmail.com>
2024-01-22 16:47:27 +0600

    headerFix

--- src/components/ui/Navbar/RightMenu.tsx
+++ src/components/ui/Navbar/RightMenu.tsx
@@ -1,27 +1,53 @@
 import { Menu, Avatar } from "antd";
 import { UserOutlined, CodeOutlined, LogoutOutlined } from "@ant-design/icons";
 import { TMode } from "./NavTypes";
+import { useAppDispatch, useAppSelector } from "../../../redux/hook";
+import { getCurrentUser, logout } from "../../../redux/features/auth/AuthSlice";
+import { useNavigate } from "react-router-dom";
 
 const RightMenu = ({ mode }: TMode) => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const user = useAppSelector(getCurrentUser);
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <Menu mode={mode}>
       <Menu.SubMenu
         title={
           <>
             <Avatar icon={<UserOutlined />} />
-            <span className="username">John Doe</span>
+            <span className="username">
+              {user?.username ? user.username : ""}
+            </span>
           </>
         }
       >
-        <Menu.Item key="project">
-          <CodeOutlined /> Projects
-        </Menu.Item>
-        <Menu.Item key="about-us">
-          <UserOutlined /> Profile
-        </Menu.Item>
-        <Menu.Item key="log-out">
-          <LogoutOutlined /> Logout
-        </Menu.Item>
+        {user?.role ? (
+          <>
+            <Menu.Item>
+              <CodeOutlined /> Projects
+            </Menu.Item>
+            <Menu.Item>
+              <UserOutlined /> Profile
+            </Menu.Item>
+            <Menu.Item onClick={handleLogout}>
+              <LogoutOutlined /> Logout
+            </Menu.Item>
+          </>
+        ) : (
+          <>
+            <Menu.Item onClick={() => navigate("/login")}>
+              <CodeOutlined /> Login
+            </Menu.Item>
+            <Menu.Item onClick={() => navigate("/register")}>
+              <CodeOutlined /> Register
+            </Menu.Item>
+          </>
+        )}
       </Menu.SubMenu>
     </Menu>
   );
